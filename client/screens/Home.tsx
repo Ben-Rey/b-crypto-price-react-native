6aea7f18d50e814e34af131aa1a0de3ad0aa52a4
fen|x <benjamin.reynes.dev@gmail.com>
2022-08-23 23:45:37 +0200

    front price

--- client/screens/Home.tsx
+++ client/screens/Home.tsx
@@ -1,21 +1,107 @@
 import React from 'react';
-import {Pressable, Text, View} from 'react-native';
+import {
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import {Crypto} from '../models/crypto';
+
+const cryptos: Crypto[] = [
+  {
+    id: '1',
+    name: 'BTC',
+    price: 22000,
+  },
+  {
+    id: '2',
+    name: 'ETH',
+    price: 1000,
+  },
+];
 
 interface HomeScreenProps {
   navigation: any;
 }
+
+const Item = ({
+  title,
+  price,
+  onPress,
+}: {
+  title: string;
+  price: number;
+  onPress: () => void;
+}) => {
+  return (
+    <Pressable onPress={onPress} style={[styles.item, styles.shadow]}>
+      <Text style={styles.itemName}>{title}</Text>
+      <Text style={styles.itemPrice}>{price} $</Text>
+    </Pressable>
+  );
+};
 
 export const HomeScreen = ({navigation}: HomeScreenProps) => {
   const navigateToDetails = () => {
     navigation.navigate('Details');
   };
 
+  const renderItem = ({item}: {item: Crypto}) => (
+    <Item onPress={navigateToDetails} title={item.name} price={item.price} />
+  );
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Pressable onPress={navigateToDetails}>
-        <Text>Navigate to Details</Text>
-      </Pressable>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={cryptos}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: '#fff4d8',
+  },
+  listContainer: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    width: '100%',
+    alignItems: 'center',
+  },
+  item: {
+    backgroundColor: '#fed468',
+    borderRadius: 8,
+    paddingVertical: 30,
+    paddingHorizontal: 25,
+    width: '80%',
+    marginVertical: 10,
+    alignSelf: 'center',
+    borderStyle: 'solid',
+    borderWidth: 3,
+    borderColor: '#5b533d',
+  },
+  itemName: {
+    color: '#5b533d',
+    fontSize: 24,
+  },
+  itemPrice: {
+    color: '#5b533d',
+    alignSelf: 'flex-end',
+  },
+  title: {
+    fontSize: 32,
+  },
+  shadow: {
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 10,
+  },
+});
